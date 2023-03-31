bd457f71ad5b34e7728858ea8db6684a1212daba
DanielRodriguez-SS <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-03-31 12:20:38 +1030

    Initial Commit

---
+++ LotSizeForexCalculator/LotSizeForexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotSizeForexCalculatorApp.swift
+//  LotSizeForexCalculator
+//
+//  Created by Daniel Rodriguez on 31/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct LotSizeForexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
