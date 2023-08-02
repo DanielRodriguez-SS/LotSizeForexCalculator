906be36d526b736fa07ace1906a76face5b58ad6
DanielRodriguez-SS <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-08-02 12:13:01 +0930

    First

--- LotSizeForexCalculator/ContentView.swift
+++ LotSizeForexCalculator/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var depositCurrency: String = ""
+    @State private var openPrice: String = ""
+    @State private var forexPair: String = ""
+    @State private var stopLoss: String = ""
+    @State private var accountBalance: String = ""
+    @State private var riskPercentage: String = ""
+    @State private var contractSize: String = "1000000"
+    @State private var theresult = 0
+    @State private var conter: Int = 0
+    
+    func increment(){
+        conter = conter + 1
+    }
+    func clear(){
+        conter = 0
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Label("Deposit Currency", systemImage: "centsign.circle")
+                TextField("AUD", text: $depositCurrency)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            HStack {
+                Label("Forex Pair", systemImage: "f.square")
+                TextField("AUDUSD", text: $forexPair)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            HStack {
+                Label("Stop Loss", systemImage: "hand.raised.fill")
+                TextField("", text: $stopLoss)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            HStack {
+                Label("Open Price", systemImage: "door.left.hand.open")
+                TextField("", text: $openPrice)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            HStack {
+                Label("Account Balance", systemImage: "dollarsign.arrow.circlepath")
+                TextField("", text: $accountBalance)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            HStack {
+                Label("Risk", systemImage: "umbrella.percent.fill")
+                TextField("2", text: $riskPercentage)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            HStack {
+                Label("Contract Size", systemImage: "batteryblock.fill")
+                TextField("1000000", text: $contractSize)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            Spacer()
+            VStack(alignment: .leading) {
+                HStack{
+                    Button("Calculate", action: increment)
+                    Button("Clear", action: clear)
+                }
+                .buttonStyle(.bordered)
+                Label(String(conter), systemImage: "bolt")
+            }
+            
         }
-        .padding()
+        .textFieldStyle(.roundedBorder)
+        .background(Color.green)
+        
     }
 }
 
